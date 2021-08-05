daef110d47f539bcd98e71bdf16cf71bb3eb237f
“Mahmoud-Abdelwahab” <mahmoudabdelwahab199390@gmail.com>
2021-08-05 06:53:54 +0200

    Update WeatherAPPTaskUITests.swift

--- WeatherAPPTaskUITests/WeatherAPPTaskUITests.swift
+++ WeatherAPPTaskUITests/WeatherAPPTaskUITests.swift
@@ -9,20 +9,50 @@ import XCTest
 
 class WeatherAPPTaskUITests: XCTestCase {
     
-    override func setUpWithError() throws {
-        continueAfterFailure = false
-    }
-    
-    override func tearDownWithError() throws {
+    func test_Search_Flow_Example() throws {
         
+        let app = XCUIApplication()
+        app.launch()
+        let searchByCityNameTextField = app.textFields["Search By City Name"]
+        
+        XCTAssertTrue(searchByCityNameTextField.exists)
+        searchByCityNameTextField.tap()
+        searchByCityNameTextField.typeText("Cairo")
+        
+        let searchButton = app.children(matching: .window).element(boundBy: 0).buttons["Search"]
+        
+        XCTAssertTrue(searchButton.exists)
+        searchButton.tap()
+        
+        let placholderText = app.staticTexts["Enter City Name"]
+        XCTAssertTrue(placholderText.exists)
+        
+        searchByCityNameTextField.tap()
+        searchByCityNameTextField.typeText("it's Not a Correct City Name")
+        searchButton.tap()
+        let emptyListTable = app.tables["Empty list"]
+        XCTAssertTrue(emptyListTable.exists)
+        emptyListTable.tap()
     }
     
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    func testGetting_Local_Weather_Case_Non_Sensetive_Search_And_AccuracyLable(){
+        
         let app = XCUIApplication()
-        //app.launch()
-        app.searchFields("")
-
+        app.launch()
+        
+        let searchByCityNameTextField = app.textFields["Search By City Name"]
+        
+        XCTAssertTrue(searchByCityNameTextField.exists)
+        searchByCityNameTextField.tap()
+        searchByCityNameTextField.typeText("as")
+        
+        let searchButton = app.children(matching: .window).element(boundBy: 0).buttons["Search"]
+        XCTAssertTrue(searchButton.exists)
+        searchButton.tap()
+        
+        let notAccurateDataStaticText = app.staticTexts["Not Accurate Data"]
+        let _ =  notAccurateDataStaticText.waitForExistence(timeout: 3)
+        XCTAssertTrue(notAccurateDataStaticText.exists,"you must be in offline mode or in any network error")
     }
     
     func testLaunchPerformance() throws {
